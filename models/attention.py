186ca5c2d2a17669b4c758aa10dc400000bdaf18
Depu Meng <depumeng@Depus-MacBook-Pro.local>
2022-05-17 11:57:26 -0400

    added support for pt1.9+

--- models/attention.py
+++ models/attention.py
@@ -26,7 +26,10 @@ from typing import Tuple, Optional
 
 import torch
 from torch import Tensor
-from torch.nn.modules.linear import _LinearWithBias
+if float(torch.__version__[:3]) < 1.9:
+    from torch.nn.modules.linear import _LinearWithBias
+else:
+    from torch.nn.modules.linear import NonDynamicallyQuantizableLinear as _LinearWithBias
 from torch.nn.init import xavier_uniform_
 from torch.nn.init import constant_
 from torch.nn.init import xavier_normal_
